5b5216821d2283825aaa43b169d17a249a9e7a85
vishakha raghuwanshi <vishakharaghuwanshi9171@gmail.com>
2026-05-25 00:27:43 +0530

    Fix file case tracking for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { StoreProvider } from './context/storeContext'
+import { StoreProvider } from './context/Store'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
