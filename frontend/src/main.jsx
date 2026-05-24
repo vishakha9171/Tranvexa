24e4c7229f8f01fadcc0b54e8693ac795c3305d4
vishakha raghuwanshi <vishakharaghuwanshi9171@gmail.com>
2026-05-25 00:51:20 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { StoreProvider } from './context/Store'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from './context/Store';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <StoreProvider>
-    <App />
-  </StoreProvider>
-    
-  </BrowserRouter>,
-)
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </BrowserRouter>
+);
