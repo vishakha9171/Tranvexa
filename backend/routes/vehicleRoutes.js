6c0792d7134d45a6217e6a294fa1bd6c0a949f33
vishakha raghuwanshi <vishakharaghuwanshi9171@gmail.com>
2026-05-26 11:32:51 +0530

    Flush structural file pathways and fix Rolldown build bottlenecks

--- backend/routes/vehicleRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { getAllVehicles, createVehicle } = require('../controllers/vehicleCtrl');
-
-router.post("/", createVehicle);
-router.get("/", getAllVehicles);
-
-module.exports = router;
